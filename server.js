be782e278cdad0568d9367122f16d93e187c6045
dmitriev dmitrii <dmitriev.dmitrii@mail.com>
2022-04-13 18:24:30 +0300

    change server config 2

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname + '/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname + '/dist/', 'index.html'));
 });
 console.log(port);
 app.listen(port);
